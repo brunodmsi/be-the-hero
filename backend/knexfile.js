1006809812bddecefa522cb9db9b3a1c9ccc9a70
Bruno De Masi <brunodemasi1@gmail.com>
2020-03-24 14:58:18 -0400

    feat(db): database configuration

---
+++ backend/knexfile.js
@@ -0,0 +1,45 @@
+require('dotenv/config');
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://postgres:docker@localhost/omnistack11',
+    migrations: {
+      directory: './src/database/migrations'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
