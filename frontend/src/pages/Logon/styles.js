2225074e28b130a584eeceddbffaeaf748a2345e
Bruno De Masi <brunodemasi1@gmail.com>
2020-03-27 15:31:33 -0400

    fix: justify-content for better spacing

--- frontend/src/pages/Logon/styles.js
+++ frontend/src/pages/Logon/styles.js
@@ -8,12 +8,12 @@ export const Container = styled.div`
 
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
 
   section {
     width: 100%;
     max-width: 350px;
-    margin-right: 200px;
+    margin-right: 30px;
 
     form {
       margin-top: 100px;
